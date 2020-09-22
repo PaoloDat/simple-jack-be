c9a516dcfb644f1868ebae764ef22998a59409da
pavel.datunashvili <pavel.datunashvili@cinimex.ru>
2020-09-22 13:43:34 +0300

    kicking off commit

---
+++ src/main/java/ge/vazisu/simplejack/SimpleJackApplication.java
@@ -0,0 +1,13 @@
+package ge.vazisu.simplejack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleJackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ge/vazisu/simplejack/SimpleJackApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.vazisu.simplejack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
